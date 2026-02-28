fe28c14daa0a57c20a9240818afd9780fc977f35
Mushfiq599 <mellowm678@gmail.com>
2026-03-01 02:17:26 +0600

    fixed payment routes

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -1,8 +1,11 @@
 import express from "express"
 import Stripe from "stripe"
+import dotenv from "dotenv"
 import Booking from "../models/Booking.model.js"
 import verifyToken from "../middleware/verifyToken.js"
 
+
+dotenv.config()
 const router = express.Router()
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
@@ -32,7 +35,7 @@ router.post("/create-payment-intent", verifyToken, async (req, res) => {
         // Amount must be in smallest currency unit (paisa = BDT * 100)
         const paymentIntent = await stripe.paymentIntents.create({
             amount: booking.serviceCost * 100,
-            currency: "bdt",
+            currency: "usd",
             metadata: {
                 bookingId: booking._id.toString(),
                 userEmail: booking.userEmail,
