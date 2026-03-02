23b276ade65d04679e3522487c3dcc7c66c2eb40
Mushfiq599 <mellowm678@gmail.com>
2026-03-03 02:58:11 +0600

    cors updated

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173",
+  "https://styledecor-server-jm4k.onrender.com"],
   credentials: true,
 }))
 app.use(express.json())
