2b2b553fc3506e5c095cf1a36c56b6342e4cd08b
Mushfiq599 <mellowm678@gmail.com>
2026-03-03 03:11:41 +0600

    update CORS with production Vercel URL

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(cors({
   origin: ["http://localhost:5173",
-  "https://styledecor-server-jm4k.onrender.com"],
+  "https://style-decor-client-five.vercel.app"],
   credentials: true,
 }))
 app.use(express.json())
